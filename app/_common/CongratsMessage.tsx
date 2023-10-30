5927f45307d67a0ae4ef51e0f1d2ba730d4249cb
Sam Huckaby <samhuckaby@gmail.com>
2023-10-30 18:24:31 -0400

    remove outdated import

--- app/_common/CongratsMessage.tsx
+++ app/_common/CongratsMessage.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import localFont from "next/font/local";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 const openDyslexic = localFont({ src: './OpenDyslexic-Regular.otf' });
 
